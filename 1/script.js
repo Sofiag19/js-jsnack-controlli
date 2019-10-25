4713c51bffee1c63aeb4f5b20f626673462a8a42
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-25 13:48:10 +0200

    init

---
+++ 1/script.js
@@ -0,0 +1,38 @@
+// L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
+
+// esplicitazione variabili
+var primoNum, secondoNum, parsePrimoNum, parseSecondoNum;
+
+// richiesta primo numero
+primoNum = parseInt(prompt("Inserisci il primo numero"));
+
+
+// richiesta secondo numero
+secondoNum = parseInt(prompt("Inserisci il secondo numero"));
+
+// output numero più alto
+if (primoNum > secondoNum) {
+  // console.log(primoNum);
+  document.getElementById('maggiore').innerHTML = "Il numero maggiore è " + primoNum;
+} else if (primoNum < secondoNum) {
+  // console.log(secondoNum);
+  document.getElementById('maggiore').innerHTML = "Il numero maggiore è " + secondoNum;
+} else {
+  // console.log("riprova ed immetti due numeri diversi");
+  document.getElementById('maggiore').innerHTML = "Riprova ed immetti due numeri diversi";
+}
+
+// if((primoNum == isNan)&&(secondoNum == isNan)){
+//   console.log("non hai inserito dei numeri");
+// } else {
+//   if (primoNum > secondoNum) {
+//     // console.log(primoNum);
+//     document.getElementById('maggiore').innerHTML = "Il numero maggiore è " + primoNum;
+//   } else if (primoNum < secondoNum) {
+//     // console.log(secondoNum);
+//     document.getElementById('maggiore').innerHTML = "Il numero maggiore è " + secondoNum;
+//   } else {
+//     // console.log("riprova ed immetti due numeri diversi");
+//     document.getElementById('maggiore').innerHTML = "Riprova ed immetti due numeri diversi";
+//   }
+// }
