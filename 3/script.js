f58b8f3b76692ae67b78fc49562dfad2d8de89f7
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-25 14:03:27 +0200

    richiesta base n5 soddisfatta

--- 3/script.js
+++ 3/script.js
@@ -2,40 +2,13 @@
 // Il programma stampa la somma di tutti i numeri inseriti.
 // Esegui questo programma in due versioni, con il for e con il while.
 
-// var num1, num2, num3, num4, num5;
-// prompts
-// num1 = parseInt(prompt("inserisci il primo numero"));
-// num2 = parseInt(prompt("inserisci il secondo numero"));
-// num3 = parseInt(prompt("inserisci il terzo numero"));
-// num4 = parseInt(prompt("inserisci il quarto numero"));
-// num5 = parseInt(prompt("inserisci il quinto numero"));
-
-// var numeriIns = [num1, num2, num3, num4, num5];
-
-// stampa somma
-// var risultato;
-
-// for
-// for (var i = 0; i < 5; i++) {
-//   risultato += numeriIns[i];
-//   console.log(risultato);
-// }
-
-// while
-// var j = 0;
-// while (j < numeriIns.length) {
-//   risultato += numeriIns[j];
-//   console.log(risultato);
-//   j++;
-// }
-
 // for
 var somma = 0;
-// for (var i = 0; i < 5; i++) {
-//   var numero = parseInt(prompt("inserisci un numero"));
-// somma += numero;
-//   console.log(somma);
-// }
+for (var i = 0; i < 5; i++) {
+  var numero = parseInt(prompt("inserisci un numero"));
+somma += numero;
+  console.log(somma);
+}
 
 // while
 var j = 0;
---
+++ 5/script.js
@@ -0,0 +1,23 @@
+// Crea un array vuoto.
+// Chiedi per sei volte all'utente di inserire un numero,
+// se è dispari inseriscilo nell'array
+
+// creazione array
+var numDisp = [];
+
+// impostazione variabili
+var numero;
+var dispari = false;
+
+// ciclare per vedere se un numero è dispari
+for (var i = 0; i < 6; i++) {
+  var numero = parseInt(prompt("inserisci un numero"));
+  if (numero%2 != 0) {
+    dispari = true;
+    numDisp.push(numero);
+  }
+}
+
+if (dispari = true) {
+  console.log(numDisp);
+}
