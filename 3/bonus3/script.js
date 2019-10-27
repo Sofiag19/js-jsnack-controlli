3a6dae0d69dc7f815d0b4456033f82b734ef822c
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-27 22:08:38 +0100

    prova bonus 3

--- 3/bonus3/script.js
+++ 3/bonus3/script.js
@@ -3,12 +3,20 @@
 // Esegui questo programma in due versioni, con il for e con il while.
 
 // for
+var risultato = document.getElementById("addizione");
 var somma = 0;
-for (var i = 0; i < 5; i++) {
-  var numero = parseInt(prompt("inserisci un numero"));
-somma += numero;
-  console.log(somma);
-}
+// var numPrec;
+
+// for (var i = 0; i < 5; i++) {
+//   var numero = parseInt(prompt("inserisci un numero"));
+//   somma += numero;
+
+  // voglio stampare tutti i numeri????
+//   numPrec += numero + " + ";
+
+
+//   risultato.innerHTML = "La somma totale dei numeri immessi è " + numPrec + " = " + somma;
+// }
 
 // while
 var j = 0;
@@ -16,5 +24,6 @@ while (j < 5) {
   var numero = parseInt(prompt("inserisci un numero"));
   somma += numero;
   console.log(somma);
+  risultato.innerHTML = "La somma totale dei numeri immessi è " + somma;
   j++;
 }
