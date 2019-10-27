14fb124272f84244b497fe7548f74115093d0ccb
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-27 22:11:53 +0100

    prova bonus 3

--- 3/bonus3/script.js
+++ 3/bonus3/script.js
@@ -1,6 +1,7 @@
 // Il software deve chiedere per 5 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 // Esegui questo programma in due versioni, con il for e con il while.
+//@ts-check
 
 // for
 var risultato = document.getElementById("addizione");
